484cfed59faf14fa100e46429ff863f2543e2f7a
Isrrael Torres Alvarado <israel.torres0@walmart.com>
2021-10-20 19:43:58 -0500

    initial commit

---
+++ CodigoBase.playground/Contents.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+//Comentarios de una linea
+//esta variable se utiliza para saludar
+
+//Comentarios de multilinea
+/*
+ Autor: Israel Torres
+ Fecha: 19 de Octubre 2021
+*/
+
+/* VARIABLES Y CONSTANTES */
+
+//Constante
+
+//declaracion implicita
+let nombre = "Israel"
+//declaracion explicita
+let apellido: String = "Torres"
+
+//Variable
+//declaracion implicita
+var edad = 36
+//declaracion explicita
+var numeroTelefono: Int = 5561632265
+
+//Concatenacion de cadenas
+print("Mi nombre es: " + nombre + "y tengo " + String(edad) + " años")
+print("Mi nombre es: \(nombre) y tengo \(edad) años")
+
+//Tipos de datos
+/*
+ Int
+ almecena valores denumeros enteros
+*/
+
+/*
+ Bool
+ almacena valores verdadero o falso y conocidos como valores boleanos
+ se ocupan cuando realizamos un control de flujo
+*/
+var esMexicano = true  //verdadero
+esMexicano = false     //falso
+
+//String
+//Character
+//Double
+//Float
+
+
+
+
+
