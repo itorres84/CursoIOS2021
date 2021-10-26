d00d53f4e7fb6be205a0c76c8c47d921480085e9
Isrrael Torres Alvarado <israel.torres0@walmart.com>
2021-10-25 19:25:36 -0500

    se agrega proyecto base UI components

---
+++ BaseProyect/BaseProyect/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BaseProyect
+//
+//  Created by Isrrael Torres Alvarado on 22/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblTitle: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lblTitle.backgroundColor = UIColor.blue
+    }
+
+
+}
+
