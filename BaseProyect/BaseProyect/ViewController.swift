d209f493a517340daa804226e866120b57416f94
Isrrael Torres Alvarado <israel.torres0@walmart.com>
2021-10-28 20:53:16 -0500

    uitexfield delegate

--- BaseProyect/BaseProyect/ViewController.swift
+++ BaseProyect/BaseProyect/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     //@IBOutlet
     //Son propiedades que nos sirven para conectar compentes de interfaz de usuario con nuestro codigo
@@ -58,9 +58,17 @@ class ViewController: UIViewController {
         btnSaludar.layer.borderColor = UIColor.black.cgColor
         btnSaludar.layer.borderWidth = 5
         
+        txtNombre.delegate = self
+        
+        let gesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.closeKeyboard))
+        self.view.addGestureRecognizer(gesture)
+        
     }
     
-    
+    @objc func closeKeyboard() {
+        txtNombre.endEditing(true)
+    }
+
     @IBAction func saludar(_ sender: UIButton) {
         
         guard let strName = txtNombre.text, !strName.isEmpty else { return }
@@ -82,5 +90,65 @@ class ViewController: UIViewController {
         print("El resultado de la suma es: \(num1 + num2)")
     }
     
+    //MARK: UITextFieldDelegate
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        changeStyleButton(state: .active)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        guard let text = textField.text else { return }
+        
+        if !text.isEmpty {
+            changeStyleButton(state: .fill)
+        } else {
+            changeStyleButton(state: .inactivo)
+        }
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        guard let text = textField.text else { return false }
+        
+        if !text.isEmpty {
+            textField.endEditing(true)
+            return true
+        } else {
+            return false
+        }
+    
+    }
+
+    
+    //MARK: Utilities view
+    func changeStyleButton(state: StateButtonViewController) {
+        
+        switch state {
+        case .active:
+            btnSaludar.backgroundColor = .yellow
+            btnSaludar.titleLabel?.textColor = .black
+            btnSaludar.isUserInteractionEnabled = false
+        case .inactivo:
+            btnSaludar.backgroundColor = UIColor.red
+            btnSaludar.titleLabel?.textColor = .white
+            btnSaludar.isUserInteractionEnabled = false
+        case .fill:
+            btnSaludar.backgroundColor = .green
+            btnSaludar.titleLabel?.textColor = .blue
+            btnSaludar.isUserInteractionEnabled = true
+        }
+        
+    }
+    
 }
+
+
+enum StateButtonViewController {
+    case active
+    case inactivo
+    case fill
+}
+
+
 
