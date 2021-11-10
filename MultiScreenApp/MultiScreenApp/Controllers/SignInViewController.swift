3faf581e70b95e868fa8ba8414bfff095543d04e
Isrrael Torres Alvarado <israel.torres0@walmart.com>
2021-11-09 21:22:40 -0600

    login flow

--- MultiScreenApp/MultiScreenApp/Controllers/SignInViewController.swift
+++ MultiScreenApp/MultiScreenApp/Controllers/SignInViewController.swift
@@ -9,21 +9,51 @@ import UIKit
 
 class SignInViewController: UIViewController {
 
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
-    
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        if let identifier = segue.identifier, identifier.elementsEqual("goToHome") {
+            
+            if let tabbar = segue.destination as? UITabBarController,
+               let homeViewController = tabbar.viewControllers?.first as? HomeViewController {
+                
+                if let name = sender as? String {
+                    homeViewController.name = name
+                }
+                
+            }
+        }
+        
     }
-    */
-
+    
+    @IBAction func login(_ sender: Any) {
+        
+        guard let strEmail = email.text, !strEmail.isEmpty,
+              let strPassword = password.text, !strPassword.isEmpty else {
+            return
+        }
+        
+        let persistenceClient = PersistenceClient()
+        
+        let name = persistenceClient.login(email: strEmail, password: strPassword)
+        
+        if !name.isEmpty, !name.elementsEqual("Error") {
+            performSegue(withIdentifier: "goToHome", sender: name)
+        }
+        
+        
+    }
+    
 }
